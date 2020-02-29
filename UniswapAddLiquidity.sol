5439e2cfab86216d733ac5104bf86f0e2ff18777
Fiona Kobayashi <57852197+fifikobayashi@users.noreply.github.com>
2020-02-29 19:59:59 +1100

    Add files via upload

---
+++ UniswapAddLiquidity.sol
@@ -0,0 +1,105 @@
+pragma solidity ^0.5.12;
+
+import './IERC20.sol';
+
+// a simple contract that interacts with Uniswap to add your assets to the liquidity pool
+contract UniswapAddLiquidity {
+
+    // Initialisation parameters for uniswap on kovan testnet
+    address kovanFactoryAddress = 0xD3E51Ef092B2845f10401a0159B2B96e8B6c3D30;
+    address kovanDaiExchange = 0x8779C708e2C3b1067de9Cd63698E4334866c691C;
+    address kovanDaiAddress = 0x4C38cDC08f1260F5c4b21685654393BB1e66a858;
+    UniswapFactoryInterface uniswapFactory = UniswapFactoryInterface(kovanFactoryAddress);
+
+    // creates an exchange on uniswap for your own ERC20 token
+    function createExchange(address token) external {
+       uniswapFactory.createExchange(token);
+    }
+   
+   // Become a liquidity provider and earn interest on your tokens
+   function addLiquidity() external payable {
+       
+       // pointer to the uniswap exchange
+       UniswapExchangeInterface uniswapExchange = UniswapExchangeInterface(
+           uniswapFactory.getExchange(kovanDaiAddress));
+       
+       // calculates Eth equivalent value
+       uint tokenAmount = uniswapExchange.getEthToTokenInputPrice(msg.value);
+       
+       // grants uniswap access to move the ERC20 token to its smart contract
+       IERC20(kovanDaiAddress).approve(address(uniswapExchange), tokenAmount);
+       
+       // executes the transfer to the contract
+       IERC20(kovanDaiAddress).transferFrom(msg.sender, address(this), tokenAmount);
+       
+       // adds your asset to the liquidity pool
+       uniswapExchange.addLiquidity.value(msg.value)(
+            tokenAmount, //min_liquidity
+            tokenAmount, //max amount of tokens you want to send
+            now + 300 // 5 mins deadline
+        );
+    }
+}
+
+// Factory interface imported from https://docs.uniswap.io/smart-contract-integration/interface
+contract UniswapFactoryInterface {
+    // Public Variables
+    address public exchangeTemplate;
+    uint256 public tokenCount;
+    // Create Exchange
+    function createExchange(address token) external returns (address exchange);
+    // Get Exchange and Token Info
+    function getExchange(address token) external view returns (address exchange);
+    function getToken(address exchange) external view returns (address token);
+    function getTokenWithId(uint256 tokenId) external view returns (address token);
+    // Never use
+    function initializeFactory(address template) external;
+}
+
+// Exchange interface imported from https://docs.uniswap.io/smart-contract-integration/interface
+contract UniswapExchangeInterface {
+    // Address of ERC20 token sold on this exchange
+    function tokenAddress() external view returns (address token);
+    // Address of Uniswap Factory
+    function factoryAddress() external view returns (address factory);
+    // Provide Liquidity
+    function addLiquidity(uint256 min_liquidity, uint256 max_tokens, uint256 deadline) external payable returns (uint256);
+    function removeLiquidity(uint256 amount, uint256 min_eth, uint256 min_tokens, uint256 deadline) external returns (uint256, uint256);
+    // Get Prices
+    function getEthToTokenInputPrice(uint256 eth_sold) external view returns (uint256 tokens_bought);
+    function getEthToTokenOutputPrice(uint256 tokens_bought) external view returns (uint256 eth_sold);
+    function getTokenToEthInputPrice(uint256 tokens_sold) external view returns (uint256 eth_bought);
+    function getTokenToEthOutputPrice(uint256 eth_bought) external view returns (uint256 tokens_sold);
+    // Trade ETH to ERC20
+    function ethToTokenSwapInput(uint256 min_tokens, uint256 deadline) external payable returns (uint256  tokens_bought);
+    function ethToTokenTransferInput(uint256 min_tokens, uint256 deadline, address recipient) external payable returns (uint256  tokens_bought);
+    function ethToTokenSwapOutput(uint256 tokens_bought, uint256 deadline) external payable returns (uint256  eth_sold);
+    function ethToTokenTransferOutput(uint256 tokens_bought, uint256 deadline, address recipient) external payable returns (uint256  eth_sold);
+    // Trade ERC20 to ETH
+    function tokenToEthSwapInput(uint256 tokens_sold, uint256 min_eth, uint256 deadline) external returns (uint256  eth_bought);
+    function tokenToEthTransferInput(uint256 tokens_sold, uint256 min_eth, uint256 deadline, address recipient) external returns (uint256  eth_bought);
+    function tokenToEthSwapOutput(uint256 eth_bought, uint256 max_tokens, uint256 deadline) external returns (uint256  tokens_sold);
+    function tokenToEthTransferOutput(uint256 eth_bought, uint256 max_tokens, uint256 deadline, address recipient) external returns (uint256  tokens_sold);
+    // Trade ERC20 to ERC20
+    function tokenToTokenSwapInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address token_addr) external returns (uint256  tokens_bought);
+    function tokenToTokenTransferInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address recipient, address token_addr) external returns (uint256  tokens_bought);
+    function tokenToTokenSwapOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address token_addr) external returns (uint256  tokens_sold);
+    function tokenToTokenTransferOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address recipient, address token_addr) external returns (uint256  tokens_sold);
+    // Trade ERC20 to Custom Pool
+    function tokenToExchangeSwapInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address exchange_addr) external returns (uint256  tokens_bought);
+    function tokenToExchangeTransferInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address recipient, address exchange_addr) external returns (uint256  tokens_bought);
+    function tokenToExchangeSwapOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address exchange_addr) external returns (uint256  tokens_sold);
+    function tokenToExchangeTransferOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address recipient, address exchange_addr) external returns (uint256  tokens_sold);
+    // ERC20 comaptibility for liquidity tokens
+    bytes32 public name;
+    bytes32 public symbol;
+    uint256 public decimals;
+    function transfer(address _to, uint256 _value) external returns (bool);
+    function transferFrom(address _from, address _to, uint256 value) external returns (bool);
+    function approve(address _spender, uint256 _value) external returns (bool);
+    function allowance(address _owner, address _spender) external view returns (uint256);
+    function balanceOf(address _owner) external view returns (uint256);
+    function totalSupply() external view returns (uint256);
+    // Never use
+    function setup(address token_addr) external;
+}
---
+++ UniswapTrade.sol
@@ -0,0 +1,93 @@
+pragma solidity ^0.5.12;
+
+// a simple contract that interacts with Uniswap to swap the ether you send with your designed ERC20 token
+contract UniswapTrade {
+
+    // Initialisation parameters for uniswap on kovan testnet
+    address kovanFactoryAddress = 0xD3E51Ef092B2845f10401a0159B2B96e8B6c3D30;
+    address kovanDaiExchange = 0x8779C708e2C3b1067de9Cd63698E4334866c691C;
+    address kovanDaiAddress = 0x4C38cDC08f1260F5c4b21685654393BB1e66a858;
+    UniswapFactoryInterface uniswapFactory = UniswapFactoryInterface(kovanFactoryAddress);
+
+    // Buys the ERC20 token from one of the liquidity pools
+    // Payable: the ether you want to trade for the ERC20 token
+    function buy() external payable {
+    
+       // pointer to the uniswap exchange
+       UniswapExchangeInterface uniswapExchange = UniswapExchangeInterface(
+           uniswapFactory.getExchange(kovanDaiAddress));
+       
+       // calculates how many of this ERC20 token you can get with the amount of ether you're sending with this buy() call
+       uint tokenAmount = uniswapExchange.getEthToTokenInputPrice(msg.value); 
+       
+       // transfers the equivalent amount of the ERC20 token to the caller of this contract
+       uniswapExchange.ethToTokenTransferInput.value(msg.value)(
+           tokenAmount, // minimum amount of tokens to receive i.e. equivalent of eth you sent
+           now + 300, // valid period of this trade (5 mins)
+           msg.sender // address of the recipient i.e. you
+       );
+   }
+}
+
+// Factory interface imported from https://docs.uniswap.io/smart-contract-integration/interface
+contract UniswapFactoryInterface {
+    // Public Variables
+    address public exchangeTemplate;
+    uint256 public tokenCount;
+    // Create Exchange
+    function createExchange(address token) external returns (address exchange);
+    // Get Exchange and Token Info
+    function getExchange(address token) external view returns (address exchange);
+    function getToken(address exchange) external view returns (address token);
+    function getTokenWithId(uint256 tokenId) external view returns (address token);
+    // Never use
+    function initializeFactory(address template) external;
+}
+
+// Exchange interface imported from https://docs.uniswap.io/smart-contract-integration/interface
+contract UniswapExchangeInterface {
+    // Address of ERC20 token sold on this exchange
+    function tokenAddress() external view returns (address token);
+    // Address of Uniswap Factory
+    function factoryAddress() external view returns (address factory);
+    // Provide Liquidity
+    function addLiquidity(uint256 min_liquidity, uint256 max_tokens, uint256 deadline) external payable returns (uint256);
+    function removeLiquidity(uint256 amount, uint256 min_eth, uint256 min_tokens, uint256 deadline) external returns (uint256, uint256);
+    // Get Prices
+    function getEthToTokenInputPrice(uint256 eth_sold) external view returns (uint256 tokens_bought);
+    function getEthToTokenOutputPrice(uint256 tokens_bought) external view returns (uint256 eth_sold);
+    function getTokenToEthInputPrice(uint256 tokens_sold) external view returns (uint256 eth_bought);
+    function getTokenToEthOutputPrice(uint256 eth_bought) external view returns (uint256 tokens_sold);
+    // Trade ETH to ERC20
+    function ethToTokenSwapInput(uint256 min_tokens, uint256 deadline) external payable returns (uint256  tokens_bought);
+    function ethToTokenTransferInput(uint256 min_tokens, uint256 deadline, address recipient) external payable returns (uint256  tokens_bought);
+    function ethToTokenSwapOutput(uint256 tokens_bought, uint256 deadline) external payable returns (uint256  eth_sold);
+    function ethToTokenTransferOutput(uint256 tokens_bought, uint256 deadline, address recipient) external payable returns (uint256  eth_sold);
+    // Trade ERC20 to ETH
+    function tokenToEthSwapInput(uint256 tokens_sold, uint256 min_eth, uint256 deadline) external returns (uint256  eth_bought);
+    function tokenToEthTransferInput(uint256 tokens_sold, uint256 min_eth, uint256 deadline, address recipient) external returns (uint256  eth_bought);
+    function tokenToEthSwapOutput(uint256 eth_bought, uint256 max_tokens, uint256 deadline) external returns (uint256  tokens_sold);
+    function tokenToEthTransferOutput(uint256 eth_bought, uint256 max_tokens, uint256 deadline, address recipient) external returns (uint256  tokens_sold);
+    // Trade ERC20 to ERC20
+    function tokenToTokenSwapInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address token_addr) external returns (uint256  tokens_bought);
+    function tokenToTokenTransferInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address recipient, address token_addr) external returns (uint256  tokens_bought);
+    function tokenToTokenSwapOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address token_addr) external returns (uint256  tokens_sold);
+    function tokenToTokenTransferOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address recipient, address token_addr) external returns (uint256  tokens_sold);
+    // Trade ERC20 to Custom Pool
+    function tokenToExchangeSwapInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address exchange_addr) external returns (uint256  tokens_bought);
+    function tokenToExchangeTransferInput(uint256 tokens_sold, uint256 min_tokens_bought, uint256 min_eth_bought, uint256 deadline, address recipient, address exchange_addr) external returns (uint256  tokens_bought);
+    function tokenToExchangeSwapOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address exchange_addr) external returns (uint256  tokens_sold);
+    function tokenToExchangeTransferOutput(uint256 tokens_bought, uint256 max_tokens_sold, uint256 max_eth_sold, uint256 deadline, address recipient, address exchange_addr) external returns (uint256  tokens_sold);
+    // ERC20 comaptibility for liquidity tokens
+    bytes32 public name;
+    bytes32 public symbol;
+    uint256 public decimals;
+    function transfer(address _to, uint256 _value) external returns (bool);
+    function transferFrom(address _from, address _to, uint256 value) external returns (bool);
+    function approve(address _spender, uint256 _value) external returns (bool);
+    function allowance(address _owner, address _spender) external view returns (uint256);
+    function balanceOf(address _owner) external view returns (uint256);
+    function totalSupply() external view returns (uint256);
+    // Never use
+    function setup(address token_addr) external;
+}
